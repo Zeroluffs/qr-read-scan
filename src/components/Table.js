6bc3be51824ea4ace20484b5b52178dfa483f751
Jhoner Pineda <39860554+Zeroluffs@users.noreply.github.com>
2022-08-17 17:03:37 -0500

    pagination

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,13 +2,26 @@ import React from "react";
 import BootstrapTable from "react-bootstrap-table-next";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-bootstrap-table-next/dist/react-bootstrap-table2.css";
+import "react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css";
+import paginationFactory from "react-bootstrap-table2-paginator";
 
 export function Table() {
   const products = [
     { id: 1, name: "George", animal: "Monkey" },
     { id: 2, name: "Jeffrey", animal: "Giraffe" },
     { id: 3, name: "Alice", animal: "Giraffe" },
-    { id: 4, name: "Alice", animal: "Tiger" },
+    { id: 4, name: "Foster", animal: "Tiger" },
+    { id: 5, name: "Tracy", animal: "Bear" },
+    { id: 6, name: "Joesph", animal: "Lion" },
+    { id: 7, name: "Tania", animal: "Deer" },
+    { id: 8, name: "Chelsea", animal: "Tiger" },
+    { id: 9, name: "Benedict", animal: "Tiger" },
+    { id: 10, name: "Chadd", animal: "Lion" },
+    { id: 11, name: "Delphine", animal: "Deer" },
+    { id: 12, name: "Elinore", animal: "Bear" },
+    { id: 13, name: "Stokes", animal: "Tiger" },
+    { id: 14, name: "Tamara", animal: "Lion" },
+    { id: 15, name: "Zackery", animal: "Bear" },
   ];
 
   const columns = [
@@ -23,9 +36,28 @@ export function Table() {
       order: "desc",
     },
   ];
+
+  const pagination = paginationFactory({
+    page: 2,
+    sizePerPage: 5,
+    lastPageText: ">>",
+    firstPageText: "<<",
+    nextPageText: ">",
+    prePageText: "<",
+    showTotal: true,
+    alwaysShowAllBtns: true,
+    onPageChange: function (page, sizePerPage) {
+      console.log("page", page);
+      console.log("sizePerPage", sizePerPage);
+    },
+    onSizePerPageChange: function (page, sizePerPage) {
+      console.log("page", page);
+      console.log("sizePerPage", sizePerPage);
+    },
+  });
   return (
     <div>
-      <h5>React Bootstrap Table Next with Sorting</h5>
+      <h5>React Bootstrap Table Next with Sorting and Pagination</h5>
 
       <BootstrapTable
         bootstrap4
@@ -33,6 +65,7 @@ export function Table() {
         data={products}
         columns={columns}
         defaultSorted={defaultSorted}
+        pagination={pagination}
       />
     </div>
   );
