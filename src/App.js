409b6d8ff6023bf525b7e3e9c9de79a43d323270
Jhoner Pineda <39860554+Zeroluffs@users.noreply.github.com>
2022-08-17 16:17:36 -0500

    table component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Auth } from "./components/Auth";
+import { Table } from "./components/Table";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/auth" element={<Auth />} />
+        <Route path="/table" element={<Table />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Table.js
@@ -0,0 +1,26 @@
+import React from "react";
+import BootstrapTable from "react-bootstrap-table-next";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export function Table() {
+  const products = [
+    { id: 1, name: "George", animal: "Monkey" },
+    { id: 2, name: "Jeffrey", animal: "Giraffe" },
+    { id: 3, name: "Alice", animal: "Giraffe" },
+    { id: 4, name: "Alice", animal: "Tiger" },
+  ];
+
+  const columns = [
+    { dataField: "id", text: "Id" },
+    { dataField: "name", text: "Name" },
+    { dataField: "animal", text: "Animal" },
+  ];
+
+  return (
+    <div>
+      <h3>React Bootstrap Table 2</h3>
+
+      <BootstrapTable keyField="id" data={products} columns={columns} />
+    </div>
+  );
+}
